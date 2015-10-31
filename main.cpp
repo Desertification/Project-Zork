24af618813a7214b6b5e8efb137fbae90d4fac5c
AlexVerschoot <alexverschoot@gmail.com>
2015-10-31 17:57:23 +0100

    put the entire interpreter in a interpreter class

--- main.cpp
+++ main.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
 #include <sstream>
 #include "room.h"
+#include "CommandInterpreter.h"
 
 using namespace std;
-
-//split the string
-std::vector<std::string> split(const std::string &text, char sep) {
-    std::vector<std::string> tokens;
-    int start = 0, end = 0;
-    while ((end = text.find(sep, start)) != std::string::npos) {
-        tokens.push_back(text.substr(start, end - start));
-        start = end + 1;
-    }
-    tokens.push_back(text.substr(start));
-    return tokens;
-}
 
 int main() {
     Room kitchen("some dark omnious kitchen\nyou feel a chill going down your spine\n");
@@ -24,42 +13,17 @@ int main() {
     kitchen.addConnection(&from_kitchen_to_cellar);
     hallway.addConnection(&from_kitchen_to_cellar);
 
-
     Room* current_room = &kitchen;
+    CommandInterpreter interpreter(current_room);
     cout << current_room->explore();
     //current_room = current_room->go("go hallway");
     //cout << current_room->explore();
 
-    //define command string, input it, then output the input
+    //define command string, and get the input
     string command;
     getline(cin, command);
-
-    //split the string
-    vector<string> stringVector= split(command, ' ');
-//  //output each word of the input
-//    for (string enkele : stringVector) {
-//        cout << enkele << endl;
-//    }
-
-    if(stringVector.front() == "go"){
-        cout << stringVector.back() << endl;
-        if (current_room->go(stringVector.back()) == NULL) {
-            cout << "No such room aviable" << endl;
-        } else {
-            current_room = current_room->go(stringVector.back());
-            cout << current_room->explore();
-        }
-    }
-
-    else{
-        cout << "No such command";
-    }
-
-
-
-
-
-
+    //interpret the input
+    interpreter.interpretInput(command);
 
     return 0;
 }
