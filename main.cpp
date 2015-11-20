76aa37cf0fd77010890c6b9e3ea53e28c2dcd2c9
AlexVerschoot <alexverschoot@gmail.com>
2015-11-20 23:40:44 +0100

    implemented the parser in the old main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
-/*#include <iostream>
+#include <iostream>
 #include <sstream>
 #include "room.h"
-#include "CommandInterpreter.h"
+#include "Living_Things/Hero.h"
+#include "Command_parser/Game.h"
+#include "Command_parser/CommandParser.h"
 
 using namespace std;
 
 
 //TODO put all this stuff in another class
 int main() {
+    CommandParser parser;
+    Game game;
+
+    //add the rooms and their connections
     Room kitchen("some dark omnious kitchen\nyou feel a chill going down your spine\n");
     Room hallway("some spooky hallway\nscreams echo in the distance\n");
     Connection from_kitchen_to_cellar (&kitchen, "door to the hallway", "hallway", &hallway, "door to the kitchen", "kitchen");
@@ -19,6 +25,12 @@ int main() {
     Monster* spider = new Monster("spider", 1, 3, 20, 10, 70,0,2);
     kitchen.addMonster(spider);
 
+    //add the commands
+    parser.registerCommand("sayhello", &Game::sayHello);
+    parser.registerCommand("saybye", &Game::sayBye);
+    parser.registerCommand("go", &Game::go);
+
+
     //the start, maybe make tutorial here, maybe even a function to load a previous game
     cout << "please enter the username you want to use" << endl;
     string username;
@@ -27,42 +39,14 @@ int main() {
 
     //the standard game
     int exit = 0;
+    //the first room
     Room* current_room = &kitchen;
-    CommandInterpreter interpreter(current_room, &exit, hero);
+    //CommandInterpreter interpreter(current_room, &exit, hero);
     cout << current_room->explore();
 
     while(exit == 0) {
-        //define command string, and get the input
-        string command;
-        getline(cin, command);
-        //interpret the input
-        interpreter.interpretInput(command);
+        //listen for context
+        parser.listen(&game);
     }
-
     return 0;
-}
- */
-
-#include <iostream>
-#include <string>
-#include <vector>
-#include <sstream>
-#include "Command_parser/Command.h"
-#include "Command_parser/CommandParser.h"
-
-using namespace std;
-
-int main(void) {
-
-    CommandParser parser;
-    Game game;
-
-    parser.registerCommand("sayhello", &Game::sayHello);
-    parser.registerCommand("saybye", &Game::sayBye);
-    parser.registerCommand("go", &Game::go);
-
-    parser.listen(&game);
-
-    return 0;
-
 }
