1217a02392baf1b408c1b0a55ab9b8bb1ff362c2
thomas <thomascauwelier@gmail.com>
2015-12-09 22:49:57 +0100

    added a library for quick end easy functions without namespace added println()

---
+++ Lib.h
@@ -0,0 +1,14 @@
+//
+// Created by thomas on 12/9/15.
+//
+
+// Dump
+
+#ifndef GAMETEST_MYLIB_H
+#define GAMETEST_MYLIB_H
+
+#include <string>
+
+void println(std::string text);
+
+#endif //GAMETEST_MYLIB_H
