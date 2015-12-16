fa14641ca50fb1b82cfef898abe25b121e5557bd
Alexander <alexander.alexander.alexander>
2015-12-16 03:16:32 +0100

    removed debug in main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,6 @@ using namespace std;
 
 int main() {
     //let the game begin
-    //startClass startClass;
-    string i2c_device = "";
-    int i2c_address = 0x40;
-    I2cNeo neoled(i2c_device, i2c_address);
-
-    while(true) {
-        neoled.blink(0,255,255,255);
-        sleep(5);
-        neoled.blink(0,255,255,255);
-        sleep(5);
-        break;
-    }
+    startClass startClass;
     return 0;
 }
